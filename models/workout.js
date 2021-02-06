a0e4c81c2e23a024119120d15d30b85587516478
Hustin <hustinkava@gmail.com>
2021-02-05 19:52:47 -0700

    model files

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Creating a new schema with the fields that we need
+const workoutSchema = new Schema({
+    // Creating a date field, mongoose accepts "Date" as a type
+    // Mongoose has a built in feature of getting the date
+    // We use Date.now and not Date.now();
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    // Creating the exercises field as an array to hold the actual exercise properties
+    exercises: [{
+        name: {
+            type: String,
+            trim: true
+        },
+        type: {
+            type: String,
+            trim: true
+        },
+        weight: {
+            type: Number,
+            default: 0
+        },
+        sets: {
+            type: Number,
+            default: 0
+        },
+        reps: {
+            type: Number,
+            default: 0
+        },
+        duration: {
+            type: Number,
+            default: 0
+        }
+    }],
+    totalDuration: {
+        type: Number,
+        default: 0
+    }
+});
+
+// Creating our table name (workout) and implementing our schema (workoutSchema)
+const workout = mongoose.model('workout', workoutSchema);
+
+module.exports = workout;
